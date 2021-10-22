cd03faeb1561983f740b67e6d99629324285c984
huangjianyong <huangjianyong@tuzhanai.com>
2021-10-22 20:20:43 +0800

    feat: daily

--- src/dancingLinks.ts
+++ src/dancingLinks.ts
@@ -44,6 +44,7 @@ class DancingLinksX extends DirectionVectors {
     throw new Error('Please run build before!');
   }
   private remove(column: number) {
+    this.throwErrorBeforeBuild();
     const { L, R, U, D, ColumnSize, Column } = this;
     (L[R[column]] = L[column]), (R[L[column]] = R[column]);
     for (let i = D[column]; i !== column; i = D[i]) {
@@ -54,6 +55,8 @@ class DancingLinksX extends DirectionVectors {
     }
   }
   private recover(column: number) {
+    this.throwErrorBeforeBuild();
+
     const { L, R, U, D, ColumnSize, Column } = this;
 
     L[R[column]] = R[L[column]] = column;
@@ -65,6 +68,8 @@ class DancingLinksX extends DirectionVectors {
     }
   }
   insert(row: number, column: number) {
+    this.throwErrorBeforeBuild();
+
     const { R, L, D, U, First, ColumnSize, Row, Column } = this;
     const idx = ++this.total;
     Row[idx] = row;
@@ -84,6 +89,8 @@ class DancingLinksX extends DirectionVectors {
     ColumnSize[column] += 1;
   }
   dance(dep = 1) {
+    this.throwErrorBeforeBuild();
+
     const { R, D, ColumnSize, Column, Row, Stack } = this;
     if (!R[0]) {
       this.ans = dep;
@@ -105,95 +112,6 @@ class DancingLinksX extends DirectionVectors {
   }
   get answer() {
     return this.Stack.slice(1, this.ans + 1);
-  }
-}
-
-function _DancingLinksX(matrix: (0 | 1)[][]) {
-  let [total] = [0, 0, 0];
-  let [first, size] = [[], []] as number[][];
-  const [U, D, L, R] = [[], [], [], []] as number[][];
-  const [row, column] = [[], []] as number[][];
-  const stack: number[] = [];
-  let ans = 0;
-  function build(r: number, c: number) {
-    for (let i = 0; i <= c; i++) {
-      L[i] = i - 1;
-      R[i] = i + 1;
-      U[i] = D[i] = i;
-    }
-    (L[0] = c), (R[c] = 0), (total = c);
-    first = Array.from({ length: r }, () => 0);
-    size = Array.from({ length: c + 1 }, () => 0);
-    total = c;
-  }
-  function insert(r: number, c: number) {
-    row[++total] = r;
-    column[total] = c;
-    size[c] += 1;
-    D[total] = D[c];
-    U[total] = c;
-    U[D[c]] = total;
-    D[c] = total;
-    if (!first[r]) {
-      first[r] = L[total] = R[total] = total;
-    } else {
-      L[total] = first[r];
-      R[total] = R[first[r]];
-      L[R[first[r]]] = total;
-      R[first[r]] = total;
-    }
-  }
-
-  function remove(c: number) {
-    R[L[c]] = R[c];
-    L[R[c]] = L[c];
-    for (let i = D[c]; i !== c; i = D[i]) {
-      for (let j = R[i]; j !== i; j = R[j]) {
-        D[U[j]] = D[j];
-        U[D[j]] = U[j];
-        size[column[j]] -= 1;
-      }
-    }
-  }
-
-  function recover(c: number) {
-    for (let i = U[c]; i !== c; i = U[i]) {
-      for (let j = L[i]; j !== i; j = L[j]) {
-        U[D[j]] = D[U[j]] = j;
-        size[column[j]] += 1;
-      }
-    }
-    L[R[c]] = R[L[c]] = c;
-  }
-
-  function dance(dep = 1) {
-    if (!R[0]) {
-      ans = dep;
-      return true;
-    }
-    let c = R[0];
-    for (let i = R[0]; i != 0; i = R[i]) {
-      if (size[c] > size[i]) c = i;
-    }
-    remove(c);
-    for (let i = D[c]; i != c; i = D[i]) {
-      stack[dep] = row[i];
-      for (let j = R[i]; j != i; j = R[j]) remove(column[j]);
-      if (dance(dep + 1)) return true;
-      for (let j = R[i]; j != i; j = R[j]) recover(column[j]);
-    }
-    recover(c);
-    return false;
-  }
-
-  build(matrix.length, matrix[0].length);
-  for (let [i, vector] of matrix.entries()) {
-    for (let [j, value] of vector.entries()) {
-      if (value) insert(i + 1, j + 1);
-    }
-  }
-  if (dance(1)) {
-    return stack.slice(1, ans + 1);
   }
 }
 
